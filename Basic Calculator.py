12821f7bf57187e08a2db15a29ef7fcd45f3bf81
Calvin <46201774+calvinjlzhai@users.noreply.github.com>
2019-04-01 22:57:31 -0400

    Add files via upload
    Updated with user input testing, changing the alternative options for the operator for multiply.

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,17 +1,17 @@
 # User input of first and second number, with their selected operator
-num1 = float(raw_input("Enter your first number: "));
-opt = raw_input("Enter your operator: ");
-num2 = float(raw_input("Enter your second number: "));
+num1 = float(input("Enter your first number: "))
+opt = input("Enter your operator: ")
+num2 = float(input("Enter your second number: "))
 
 
 # output based on user input
 if opt == "+":
-    print (num1 + num2);
+    print (num1 + num2)
 elif opt == "-":
-    print (num1 - num2);
+    print (num1 - num2)
 elif opt == "/":
-    print (num1 / num2);
-elif opt == "*":
-    print (num1 * num2);
+    print (num1 / num2)
+elif opt == "*" or opt == "x" or opt == "X":
+    print (num1 * num2)
 else:
-    print ("Invalid operator");
+    print ("Invalid operator")
